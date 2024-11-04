ca748f7ac7bbcfd9041bf88906d6d99225b0e3a3
NicolasOlmos222 <nicolas0olmos@gmail.com>
2024-11-04 10:05:37 -0300

    testing

--- test.js
+++ test.js
@@ -2,21 +2,25 @@ import { Estudiante } from "./clases/classEstudiante.js";
 import { Profesor } from "./clases/classProfesor.js";
 import { Curso } from "./clases/classCurso.js";
 
-
-
+//Inicializo clases
 var estudiante = new Estudiante("Juan", "Perez", 123456, "Matematicas");
 var profesor = new Profesor("Pedro", "Lopez", "Matematicas", "pedro@pedro.com");
 var curso = new Curso("5", "B")
 
+//Muestro informacion
 alert(estudiante.getInfo());
 alert(profesor.getInfo());
 alert(curso.getInfo());
 
-
-curso.agregarEstudiante(estudiante);
-alert(curso.listarEstudiantes());
+//Agrego un objeto estudiante a curso
 curso.agregarEstudiante(estudiante);
 alert(curso.listarEstudiantes());
 
+//Inicializo otro objeto estudiante y lo guardo
+var estudiante = new Estudiante("NATALIA", "Perez", 123456, "Matematicas");
+curso.agregarEstudiante(estudiante);
+alert(curso.listarEstudiantes());
+
+//Asigno un profesor
 curso.asignarProfesor(profesor);
 alert(curso.getInfo());
