db168966a57d06f1cc79cf56c1e7fe10da8f1e36
NicolasOlmos222 <nicolas0olmos@gmail.com>
2024-11-11 09:55:59 -0300

    Agregue css

--- script.js
+++ script.js
@@ -2,10 +2,19 @@ var estudiante = new Estudiante("Juan", "Perez", 123456, "Matematicas");
 var profesor = new Profesor("Pedro", "Lopez", "Matematicas", "pedro@pedro.com");
 var curso = new Curso("5", "B")
 
-function miFuncion() {
-    alert(estudiante.getInfo());
-}
 
-// Obtener el botón y agregar el evento de clic
-const boton = document.getElementById('hola');
-boton.addEventListener('click', miFuncion);
+function mostrarSeccion(seccionId) {
+    if (seccionId == "estudiante"){
+        document.getElementById("estudiante").style.display = "block";
+        document.getElementById("profesor").style.display = "none";
+        document.getElementById("curso").style.display = "none";
+    } else if (seccionId == "profesor"){
+        document.getElementById("estudiante").style.display = "none";
+        document.getElementById("profesor").style.display = "block";
+        document.getElementById("curso").style.display = "none";
+    } else{
+        document.getElementById("estudiante").style.display = "none";
+        document.getElementById("profesor").style.display = "none";
+        document.getElementById("curso").style.display = "block";
+    }
+}
